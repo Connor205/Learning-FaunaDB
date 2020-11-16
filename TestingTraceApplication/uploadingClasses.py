7c3284c0f3470082a9327c9654c398b68109d3f3
connor205 <Conn205@gmail.com>
2020-11-16 10:32:17 -0500

    Creating uploading classes to fauna from json

--- TestingTraceApplication/uploadingClasses.py
+++ TestingTraceApplication/uploadingClasses.py
@@ -1,14 +1,32 @@
 from faunadb import query as q
 from faunadb.objects import Ref
 from faunadb.client import FaunaClient
+from tqdm import tqdm
 import pprint
 import json
 
 f = open(".faunarc", "r")
 testDatabaseKey = f.readline()[10:]
 f.close()
-print(testDatabaseKey)
+# print(testDatabaseKey)
 
 client = FaunaClient(testDatabaseKey)
 
-indexes = client.query(q.paginate(q.indexes()))
+# indexes = client.query(q.paginate(q.indexes()))
+# pprint.pprint(indexes)
+
+with open("classes.txt", "r") as f:
+    classes_dict = json.load(f)
+
+# print(list(classes_dict.keys()))
+
+for c in tqdm(list(classes_dict.keys())[5:]):
+    data_dict = {
+        "identifier": classes_dict[c]["course_abbrev"],
+        "course_num": classes_dict[c]["course_number"],
+        "full_identifier": c,
+        "department": classes_dict[c]["department"],
+        "description": classes_dict[c]["description"],
+        "credit_hours": classes_dict[c]["credit_hours"]
+    }
+    client.query(q.create(q.collection("Class"), {"data": data_dict}))
